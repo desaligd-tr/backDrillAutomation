787895b9824d9f4bccafebc0c2498e13d2063699
Desali Gdalo <desaligd@etoro.com>
2023-04-16 09:04:26 +0300

    chages of IList

--- backDrillAutomation/practice.cs
+++ backDrillAutomation/practice.cs
@@ -85,56 +85,117 @@ namespace backDrillAutomation
             IWebElement btc = driver.FindElement(By.XPath("(//div[@class='symbol'])[1]"));
             Thread.Sleep(2000);
             btc.Click();
-            Console.WriteLine("Succesfful");
+            //Console.WriteLine("Succesfful");
             //IWebElement click_btc = driver.FindElement(By.XPath("//*[@id=watchlist - instruments]/div[2]/div[1]/div/div[1]/div/et-card-avatar/a/div[2]/div[2]"));
             //click_btc.Click();
            // Console.WriteLine("Succesfful");
-            //Thread.Sleep(2000);
-            // IWebElement button_trade = driver.FindElement(By.Id("Thread.Sleep(2000)"));
-            // button_trade.Click();
+            Thread.Sleep(2000);
+            IWebElement button_trade = driver.FindElement(By.XPath("//div[@class='head-instrument-action']/et-market-page-trade-button"));
+             button_trade.Click();
+            // IWebElement cash_input = driver.FindElement(By.Id("input"));
+            //cash_input.SendKeys("10");
+            Thread.Sleep(2000);
+            IWebElement input_tradeAmount = driver.FindElement(By.XPath("(//input[starts-with(@type,'text')])[2]"));
+            Thread.Sleep(1000);
+            input_tradeAmount.Clear();
+            Thread.Sleep(2000);
+            input_tradeAmount.SendKeys("10");
+            Thread.Sleep(2000);
+            IWebElement btn_openTrade = driver.FindElement(By.XPath("//div[@class='execution-action-button']/button"));
+            btn_openTrade.Click();
+            Console.WriteLine("Succesfful");
+            Thread.Sleep(2000);
+        }
+
+        
+        [Test                                     ]
+        public void test01_loginVPN()
+        {
+            driver.Url = "https://offlinemode.etoro.com";
+            driver.Manage().Window.Maximize();
+            IWebElement userName = driver.FindElement(By.Id("username"));
+            IWebElement password = driver.FindElement(By.Id("password"));
+            userName.SendKeys("BetaTester30");
+            password.SendKeys("Aa123456");
+            IWebElement btn_login = driver.FindElement(By.Id("submit-button"));
+            btn_login.Click();
+            Thread.Sleep(3000);
+            IWebElement btn_popUp = driver.FindElement(By.Id("warning-modal-continue"));
+            btn_popUp.Click();
+            Thread.Sleep(3000);
+            IWebElement btn_logOut = driver.FindElement(By.XPath("//div[starts-with(@class,'header__logout')]"));
+            Assert.AreEqual(btn_logOut.Text, "Logout");
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            IList<IWebElement> elements = driver.FindElements(By.XPath("//span[starts-with(@class,'first__item__subheading ng')]"));
+            IWebElement element = elements[0];
+            Console.WriteLine("Printed"+element.Text);
+            //Console.WriteLine("Succesfful");
+            IWebElement dropDown_request = driver.FindElement(By.XPath("(//select[starts-with(@id,'select-')])[1]"));
+            dropDown_request.Click();
+            Thread.Sleep(2000);
+            IWebElement option_requestToClose = driver.FindElement(By.XPath("(//select[starts-with(@id,'select-')])[1]/option[2]"));
+            option_requestToClose.Click();
+            Console.WriteLine("Selected");
+            Thread.Sleep(2000);
+            IWebElement sumbit_btn = driver.FindElement(By.Id("summary-bar-submit-button"));
+            Thread.Sleep(3000);
+            sumbit_btn.Click();
+            //IWebElement requestclose = driver.FindElement(By.Id("select-()"));
+            //requestclose.Click();
+            //  Thread.Sleep(3000);
+            //IWebElement option2 = driver.FindElement(By.CssSelector("#select-() > option:nth-child(2)"));
+            //option2.Click();
+            //IWebElement sumbit_btn = driver.FindElement(By.Id("summary-bar-submit-button"));
+            //Thread.Sleep(3000);
+            //sumbit_btn.Click();
+            //IWebElement confirm_btn = driver.FindElement(By.Id("prompt-modal-continue"));
+            //confirm_btn.Click();
+            //IWebElement close_request = driver.FindElement(By.XPath("//*[@id=row-2413225501-request]/span"));
+            //Assert.AreEqual(close_request.Text, "Close Requested");
             //Console.WriteLine("Succesfful");
         }
+
     }
 
-/*[Test]
-public void test02_addToWatchList()
-{
-    driver.Url = "https://www.etoro.com/login";
-    driver.Manage().Window.Maximize();
-    IWebElement userName = driver.FindElement(By.Id("username"));
-    IWebElement password = driver.FindElement(By.Id("password"));
-    userName.SendKeys("BetaTester30");
-    password.SendKeys("Aa123456");
-    String userNameText = userName.Text;
-    IWebElement btn_login = driver.FindElement(By.XPath("//button[@class='button-default blue-btn']"));
-    btn_login.Click();
-    Thread.Sleep(3000);
-    IWebElement nameProfile = driver.FindElement(By.XPath("//div[@automation-id='sidenav-user-box']"));
-    Assert.True(nameProfile.Displayed);
-    Thread.Sleep(1000);
-    IWebElement btn_watchList = driver.FindElement(By.XPath("//a[@automation-id='sidenav-menu-watchlists']"));
-    btn_watchList.Click();
-    Thread.Sleep(3000);
-    IWebElement btn_addToWatchList = driver.FindElement(By.XPath("//button[@automation-id='watchlist-watchlist-btn-add-markets']"));
-    btn_addToWatchList.Click();
-    IWebElement btn_searchFromWatchList = driver.FindElement(By.XPath("//input[@automation-id='add-markets-search-bar']"));
-    btn_searchFromWatchList.SendKeys("doge");
-    Thread.Sleep(3000);
-    IWebElement btn_addToWatchL = driver.FindElement(By.XPath("//ets-checkbox[@automation-id='add-markets-browser-btn-add-item']"));
-    btn_addToWatchL.Click();
-    IWebElement btn_done = driver.FindElement(By.XPath("//button[@automation-id='add-markets-close-dialog-btn']"));
-    btn_done.Click();
-    Thread.Sleep(3000);
-    IWebElement list_symbol = driver.FindElement(By.XPath("(//div[@class='symbol'])[1]"));
-    Assert.AreEqual("DOGE", list_symbol.Text);
-    btn_addToWatchList.Click();
-    Thread.Sleep(3000);
-    IWebElement btn_searchFromWatchListR = driver.FindElement(By.XPath("//input[@automation-id='add-markets-search-bar']"));
-    btn_searchFromWatchListR.SendKeys("doge");
-    Thread.Sleep(2000);
-    IWebElement btn_removeFromWatchL = driver.FindElement(By.XPath("//ets-checkbox[@automation-id='add-markets-browser-btn-add-item']"));
-    btn_removeFromWatchL.Click();
-}
-*/
-       
+    /*[Test]
+    public void test02_addToWatchList()
+    {
+        driver.Url = "https://www.etoro.com/login";
+        driver.Manage().Window.Maximize();
+        IWebElement userName = driver.FindElement(By.Id("username"));
+        IWebElement password = driver.FindElement(By.Id("password"));
+        userName.SendKeys("BetaTester30");
+        password.SendKeys("Aa123456");
+        String userNameText = userName.Text;
+        IWebElement btn_login = driver.FindElement(By.XPath("//button[@class='button-default blue-btn']"));
+        btn_login.Click();
+        Thread.Sleep(3000);
+        IWebElement nameProfile = driver.FindElement(By.XPath("//div[@automation-id='sidenav-user-box']"));
+        Assert.True(nameProfile.Displayed);
+        Thread.Sleep(1000);
+        IWebElement btn_watchList = driver.FindElement(By.XPath("//a[@automation-id='sidenav-menu-watchlists']"));
+        btn_watchList.Click();
+        Thread.Sleep(3000);
+        IWebElement btn_addToWatchList = driver.FindElement(By.XPath("//button[@automation-id='watchlist-watchlist-btn-add-markets']"));
+        btn_addToWatchList.Click();
+        IWebElement btn_searchFromWatchList = driver.FindElement(By.XPath("//input[@automation-id='add-markets-search-bar']"));
+        btn_searchFromWatchList.SendKeys("doge");
+        Thread.Sleep(3000);
+        IWebElement btn_addToWatchL = driver.FindElement(By.XPath("//ets-checkbox[@automation-id='add-markets-browser-btn-add-item']"));
+        btn_addToWatchL.Click();
+        IWebElement btn_done = driver.FindElement(By.XPath("//button[@automation-id='add-markets-close-dialog-btn']"));
+        btn_done.Click();
+        Thread.Sleep(3000);
+        IWebElement list_symbol = driver.FindElement(By.XPath("(//div[@class='symbol'])[1]"));
+        Assert.AreEqual("DOGE", list_symbol.Text);
+        btn_addToWatchList.Click();
+        Thread.Sleep(3000);
+        IWebElement btn_searchFromWatchListR = driver.FindElement(By.XPath("//input[@automation-id='add-markets-search-bar']"));
+        btn_searchFromWatchListR.SendKeys("doge");
+        Thread.Sleep(2000);
+        IWebElement btn_removeFromWatchL = driver.FindElement(By.XPath("//ets-checkbox[@automation-id='add-markets-browser-btn-add-item']"));
+        btn_removeFromWatchL.Click();
+    }
+    */
+
 }
